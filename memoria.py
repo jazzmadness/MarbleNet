2414d2446b7a3818879e7e461b1326bc87b95a3e
Leonardo Torin <leonardo_torin@yahoo.com.br>
2018-12-02 12:29:44 -0200

    Classe para guardar experiencias anteriores para serem amostradas e usadas nos mini batchs do SGD

---
+++ memoria.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+'''
+Cria um deque, i.e, um array de tamanho maximo definido pelo buffer, que quando eh adicionado um elemento novo,
+ele substitui o elemento mais antigo do array.
+
+A memoria eh usada para guardar experiencias para rodar o Stochastic Gradient Descent
+'''
+
+class Memoria():
+	def __init__(self, buf):
+		self.buffer = deque(maxlen = buf)
+
+	def add(self, exp):
+		self.buffer.append(exp)
+	def amostra(self, batch):
+		return [self.buffer[i] for i in np.random.choice(
+														np.arrange(
+																	len(self.buffer)
+																	), 
+														size = batch,
+														replace = False
+														)
+				]
