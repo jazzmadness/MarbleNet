ede514e3cdba23e703fd9ec6b2870c888632ef6a
Leonardo Torin <leonardo_torin@yahoo.com.br>
2018-12-02 14:29:54 -0200

    Modificando o nome da classe

--- memoria.py
+++ memoria.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import deque
 
 '''
 Cria um deque, i.e, um array de tamanho maximo definido pelo buffer, que quando eh adicionado um elemento novo,
@@ -7,7 +8,7 @@ ele substitui o elemento mais antigo do array.
 A memoria eh usada para guardar experiencias para rodar o Stochastic Gradient Descent
 '''
 
-class Memoria():
+class Mem():
 	def __init__(self, buf):
 		self.buffer = deque(maxlen = buf)
 
